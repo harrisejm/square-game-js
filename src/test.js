ee105d604846834c00d732435126cc9afed2a5f3
Eddie Harris <harrisejm@gmail.com>
2018-08-01 23:34:05 -0700

    add

--- src/test.js
+++ src/test.js
@@ -12,6 +12,7 @@ var tempArr = test.slice();
 
 let arr1 = [];
 let arr2 = [];
+let arrComb = [arr1, arr2];
 
 function fillArr(){
   if (tempArr.length !== 0) {
@@ -27,20 +28,28 @@ function fillArr(){
   }
 }
 
-function fillArr2() {
+ function fillArr2() {
+
+
   if (tempArr.length !== 0) {
     for (var i = 0; i < test.length; i++) {
-      let removeIndex1 = tempArr.indexOf(arr1[i]);
-      let remove1 = tempArr.splice(removeIndex1, 1);
-      let selection = tempArr[Math.floor(Math.random()*(tempArr.length))];
-      if (selection === undefined) {
-        arr2.push(remove1);
-      } else {
-        arr2.push(selection);
-      }
-      tempArr.push(remove1);
-      let remove = tempArr.indexOf(selection);
-      tempArr.splice(remove, 1);
+
+  let removeIndex1 = tempArr.indexOf(arr1[i]);
+
+  let remove1 = tempArr.splice(removeIndex1, 1);
+
+
+    let selection = tempArr[Math.floor(Math.random()*(tempArr.length))];
+    if (selection === undefined) {
+     arr2.push(remove1);
+    } else {
+    arr2.push(selection);
+    }
+    tempArr.push(remove1);
+
+    let remove = tempArr.indexOf(selection);
+    tempArr.splice(remove, 1);
+
     }
     fillArr2();
   }
@@ -48,6 +57,25 @@ function fillArr2() {
 
 fillArr();
 
+ var tempArr2 = [];
+for (var q = 0; q < arrComb.length; q++) {
+  for (var y = 0; y < arrComb.length+1; y++) {
+    if (y%3 === 0) {
+      tempArr2.push(" ");
+      tempArr2.push(arrComb[q][y]);
+    } else {
+      tempArr2.push(arrComb[q][y]);
+    }
+  }
+}
+var testSmall = tempArr2.join("").trim().split(" ");
+var arr3 = [];
+
+for (var r = 0; r < testSmall.length; r++) {
+  arr3.push(testSmall[r].split(""));
+}
+
+
 document.getElementById("test").innerHTML = arr1;
 document.getElementById("test1").innerHTML = arr2;
-document.getElementById("test2").innerHTML = arr2[8];
+document.getElementById("test2").innerHTML = testSmall;
